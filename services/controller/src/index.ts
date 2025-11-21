7fb3fcdf9aa8addd19d2d1cb37c1ad01d736ea4a
David Zeff <dpzeff@gmail.com>
2025-11-21 11:08:01 +0200

    .

---
+++ services/controller/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.get("/health", (req, res) => {
+  res.send({ health: "up" });
+});
+
+app.post("/api/locations", (req, res) => {
+  const location = req.body.location;
+  console.log(location.lat, location.lng);
+  res.send(req.body);
+});
+
+app.listen(3000, () => {
+  console.log(`listening on 3000`);
+});
