f93bd9deae0d7980a89cbc903e7098eadbbfa851
David Zeff <dpzeff@gmail.com>
2025-11-21 14:53:22 +0200

    .

--- services/controller/src/tools/swagger.js
+++
@@ -1,6 +0,0 @@
-const swaggerAutogen = require("swagger-autogen")();
-
-const outputFile = "./swagger_output.json";
-const endpointsFiles = ["../../index.ts"];
-
-swaggerAutogen(outputFile, endpointsFiles);
